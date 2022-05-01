fdf9b60cf409441417a1e3691a315002fadbda16
Woojin <wj0017@naver.com>
2022-05-02 02:01:37 +0900

    11720

---
+++ Baekjoon/11720.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n; 
+    char ch;
+ 
+    cin >> n;
+ 
+    int total = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ch;
+        total += ch - 48;  
+    }
+ 
+    cout << total << endl;
+ 
+    return 0;
+}
+
