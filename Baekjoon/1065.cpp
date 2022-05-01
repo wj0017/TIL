35fdf570a9cbfbf3a1e17cf2e631f2777f8e5b51
Woojin <wj0017@naver.com>
2022-05-02 02:00:31 +0900

    1065

---
+++ Baekjoon/1065.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+
+bool hansoo(int num){
+
+    if(num < 100)
+        return true;
+
+    else if(num == 1000)
+        return false;
+
+    else{
+        int data[4];
+
+        for(int i = 0; i < 3; i++){
+
+            if(num == 0){
+                break;
+            }
+            int decom = num % 10;
+            data[i] = decom;
+            num /= 10;
+        }
+
+        if(data[2] - data[1] == data[1] - data[0])
+            return true;
+        return false;
+    }
+    
+}
+
+int main(){
+
+    int num;
+    int cnt = 0;
+    cin >> num;
+    
+    for(int i = 1; i <= num; i++){
+        if(hansoo(i))
+            cnt++;
+        }
+    
+    cout << cnt;
+}
