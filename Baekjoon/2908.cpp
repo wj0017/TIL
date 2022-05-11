2715643fb5495beb0f9b082ef08eb18e906d9801
Woojin <wj0017@naver.com>
2022-05-11 19:15:55 +0900

    2908

--- Baekjoon/2908.cpp
+++ Baekjoon/2908.cpp
@@ -27,3 +27,29 @@ int main(void) {
 
     return 0;
 }
+
+/* 이렇게 하는 게 더 효율 적일 듯
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	string x, y;
+	cin >> x >> y;
+
+	string bigger;
+
+	for (int i = 2; i >= 0; i--) {
+		if (x[i] > y[i]) {
+			bigger = x; break;
+		}
+		else if (x[i] < y[i]) {
+			bigger = y; break;
+		}
+	}
+
+	cout << bigger[2] << bigger[1] << bigger[0];
+	return 0;
+}
+*/
