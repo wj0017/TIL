a04bb158558a70b0b891e5d7af7f88bac6a6fd8c
Woojin <wj0017@naver.com>
2022-07-08 15:38:50 +0900

    1874

---
+++ Baekjoon/1874.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+ 
+using namespace std;
+
+int main(){
+
+    stack<int> a;
+    vector<char> b;
+
+    int num, num_ = 1;
+    cin >> num;
+
+    while(num--){
+        int c;
+        cin >> c;
+
+        while ( num_ <= c )
+		{
+			a.push(num_);
+			b.push_back('+');
+            num_++;
+		}
+
+        if (a.top() == c)
+		{
+			a.pop();
+			b.push_back('-');
+		}
+		else
+		{
+			cout << "NO";
+			return 0;
+		}
+	}
+ 
+	for (int i = 0; i < b.size(); i++)
+	{
+		cout << b[i] << '\n';
+	}
+}
