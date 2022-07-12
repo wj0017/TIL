01ac4e0a48c7d46ffca28572fa613ed8918481e4
Woojin <wj0017@naver.com>
2022-07-12 09:00:09 +0900

    17413

---
+++ Baekjoon/17413.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+void fast_io(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+int main(void)
+{
+	fast_io();
+	stack<char> s;
+	string str;
+	int i = 0;
+	getline(cin, str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '<')
+		{
+			while (!s.empty())
+			{
+				cout << s.top();
+				s.pop();
+			}
+			while (true)
+			{
+				cout << str[i];
+				if (str[i] == '>')
+					break;
+				i++;
+			}
+		}
+		else if (isalnum(str[i]))
+		{
+			s.push(str[i]);
+		}
+		else
+		{
+			while (!s.empty())
+			{
+				cout << s.top();
+				s.pop();
+			}
+			cout << " ";
+		}
+		i++;
+	}
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}
+}
