b06716ece8cd9e7afec488cb0b1cee2819d57ad7
Woojin <wj0017@naver.com>
2022-07-12 09:00:35 +0900

    17299

---
+++ Baekjoon/17299.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<stack>
+#define MAX 1000001
+
+using namespace std;
+
+int main(void)
+{
+	stack<int> st;
+    int arr[MAX];
+    int result[MAX];
+    int num, cnt;
+    
+    cin >> num;
+
+    for(int i = 0; i < num; i++){cin >> arr[i];}
+
+    for(int i = num - 1; i >= 0; i--){
+        for(int j = 0; j < st.size(); j++){
+            
+        }
+        while (!st.empty() && ){st.pop();}
+
+        if(st.empty()) result[i] = -1;
+        else result[i] = st.top();
+
+        st.push(arr[i]);
+    }
+
+    for(int i = 0; i < num; i++)
+        cout << result[i] << " ";
+}
