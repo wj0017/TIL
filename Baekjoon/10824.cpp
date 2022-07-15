99f82840f02e8a7d59f56bf87afc1f4e14d1e3f4
Woojin <wj0017@naver.com>
2022-07-15 15:37:31 +0900

    10824

---
+++ Baekjoon/10824.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string a, b, c, d;
+	long long num1, num2 = 0;	
+
+	cin >> a >> b >> c >> d;
+
+	a += b;	
+	c += d;	
+
+	num1 = stoll(a);
+	num2 = stoll(c);
+
+	cout << num1 + num2;	
+
+	return 0;
+}
