e5b43dde82dcc1eb3b694eab80e0382cb8049fa3
Woojin <wj0017@naver.com>
2022-07-15 15:36:53 +0900

    1918

--- Baekjoon/1918.cpp
+++ Baekjoon/1918.cpp
@@ -1,27 +1,67 @@
 #include <iostream>
 #include <stack>
-#include <vector>
 using namespace std;
+
+int priority(char c)
+{
+	switch (c)
+	{
+	case '(':
+		return 0;
+	case '+':
+	case '-':
+		return 1;
+	case '*':
+	case '/':
+		return 2;
+	default:
+		break;
+	}
+}
 
 int main()
 {
-	cin.tie(0);
-	ios_base::sync_with_stdio(0);
-
+	stack<char> op;
 	string exp;
-    stack<char> st;
-
 	cin >> exp;
 
-    for(int i = 0; i < exp.length(); i++){
-        if (exp[i] >= 'A' && exp[i] <= 'Z'){ st.push(exp[i]); }
-        else{
-            if(exp[i] == '('){
-                   
-            }
-        }
-    }
+	for (int i = 0; i < exp.length(); i++)
+	{
+		if (exp[i] >= 'A' && exp[i] <= 'Z')
+		{
+			cout << exp[i];
+		}
+		else if (exp[i] == '(')
+		{
+			op.push('(');
+		}
+		else if (exp[i] == ')')
+		{
+			while (op.top() != '(')
+			{
+				cout << op.top();
+				op.pop();
+			}
+			op.pop();
+		}
+		else
+		{
+			while (!op.empty() && priority(op.top()) >= priority(exp[i]))
+			{
+				cout << op.top();
+				op.pop();
+			}
+			op.push(exp[i]);
+		}
+		
+	}
 
+	while (!op.empty())
+	{
+		cout << op.top();
+		op.pop();
+	}
+	cout << '\n';
 
 	return 0;
 }
