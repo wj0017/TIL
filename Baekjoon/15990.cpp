ca34d284d95589435e60d8fdcc8c026be817d52c
wj0017 <wj0017@naver.com>
2025-03-26 17:38:46 +0900

    update 15990.cpp

--- Baekjoon/15990.cpp
+++ Baekjoon/15990.cpp
@@ -14,13 +14,66 @@
 
 출력
 각 테스트 케이스마다, n을 1, 2, 3의 합으로 나타내는 방법의 수를 1,000,000,009로 나눈 나머지를 출력한다.
+
+1-100,000
+
+1
+2
+3 12 21
+4 121 31 13
+5 1212 2121 32 23
+6 
+
+
 */
 
-#include<iostream>
-#include<queue>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-  
+const int MOD = 1000000009;
+const int MAX_N = 100000;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // 전처리: 모든 가능한 n에 대해 미리 계산
+    vector<vector<long long>> dp(MAX_N + 1, vector<long long>(4, 0));
+    
+    // 초기값 설정
+    dp[1][1] = 1;  // 1
+    dp[2][2] = 1;  // 2
+    dp[3][3] = 1;  // 3
+    dp[3][1] = 1;  // 2+1
+    dp[3][2] = 1;  // 1+2
+    
+    for (int i = 4; i <= MAX_N; i++) {
+        // 마지막 숫자가 1인 경우, 이전 숫자는 2나 3이어야 함
+        dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % MOD;
+        
+        // 마지막 숫자가 2인 경우, 이전 숫자는 1이나 3이어야 함
+        dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % MOD;
+        
+        // 마지막 숫자가 3인 경우, 이전 숫자는 1이나 2이어야 함
+        dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % MOD;
+    }
+    
+    // 각 n에 대한 결과를 미리 계산
+    vector<int> result(MAX_N + 1);
+    for (int i = 1; i <= MAX_N; i++) {
+        result[i] = (dp[i][1] + dp[i][2] + dp[i][3]) % MOD;
+    }
+    
+    // 테스트 케이스 처리
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << result[n] << '\n';
+    }
+    
+    return 0;
 }
